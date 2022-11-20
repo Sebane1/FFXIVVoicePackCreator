a2480c79ec9195db8bf02c2d15d4e0c0916d8590
Sebastina <sebane1@thavea.com>
2022-11-19 22:05:15 -0600

    Add meta editing, and general user interface improvements.

--- FFXIVVoiceClipNameGuesser/Form1.cs
+++ FFXIVVoiceClipNameGuesser/Form1.cs
@@ -17,9 +17,11 @@ namespace FFXIVVoiceClipNameGuesser {
         private string dumpFilepath;
         private List<string> files;
         private List<string> secondaryKnownFileList;
+        private List<string> lostFileListPaths;
         private Point startPos;
         private bool canDoDragDrop;
         private SCDGenerator scdGenerator;
+        private string metaFilePath;
 
         public MainWindow() {
             InitializeComponent();
@@ -31,22 +33,10 @@ namespace FFXIVVoiceClipNameGuesser {
         }
 
         private void Form1_Load(object sender, EventArgs e) {
-            bool dontLoad = false;
+            Text += " " + Application.ProductVersion;
             missingFIleList.Items.Clear();
             if (MessageBox.Show(@"Do you have voices dumped via FFXIVExplorer or an equivalent tool as .wav files?", "FFXIV Voice Clip Name Guesser", MessageBoxButtons.YesNo) == DialogResult.Yes) {
-                if (dumpFilepath == null) {
-                    MessageBox.Show(@"Please select the folder with your dumped voice files. Should be under ""vo_emote"".", "FFXIV Voice Clip Name Guesser");
-                    FolderSelectDialog openFileDialog = new FolderSelectDialog();
-                    if (openFileDialog.ShowDialog() == DialogResult.OK) {
-                        dumpFilepath = openFileDialog.SelectedPath;
-                    } else {
-                        MessageBox.Show("No dumped voice folder selected, voice guessing will be hindered.", "FFXIV Voice Clip Name Guesser");
-                        dontLoad = true;
-                    }
-                }
-                if (!dontLoad) {
-                    PickVoiceDump();
-                }
+                PickVoiceDump();
             } else {
                 MessageBox.Show("Voice guessing will be hindered without a dump folder selected.", "FFXIV Voice Clip Name Guesser");
             }
@@ -54,52 +44,69 @@ namespace FFXIVVoiceClipNameGuesser {
         }
 
         private void PickVoiceDump() {
-            missingFIleList.Items.Clear();
-            if (files != null) {
-                files.Clear();
+            bool dontLoad = false;
+            MessageBox.Show(@"Please select the folder with your dumped voice files. Should be under ""vo_emote"".", "FFXIV Voice Clip Name Guesser");
+            FolderSelectDialog openFileDialog = new FolderSelectDialog();
+            if (openFileDialog.ShowDialog() == DialogResult.OK) {
+                dumpFilepath = openFileDialog.SelectedPath;
+            } else {
+                MessageBox.Show("No dumped voice folder selected", "FFXIV Voice Clip Name Guesser");
+                dontLoad = true;
             }
-            files = new List<string>();
-            files.AddRange(Directory.GetFiles(dumpFilepath));
-            if (secondaryKnownFileList != null) {
-                secondaryKnownFileList.Clear();
-            }
-            secondaryKnownFileList = new List<string>();
-            int startValue = 7301000;
-            int index = 0;
-            int attempts = (files.Count / 16);
-            bool firstUnfound = false;
-            int foundVoices = 0;
-            int unfoundVoices = 0;
-            for (int counter = 0; counter <= attempts; counter++) {
-                bool itemsNotFound = false;
-                for (int emoteCount = 0; emoteCount <= 15; emoteCount++) {
-                    string fileName = (startValue + emoteCount) + "";
-                    string name = ListContainsName(files, fileName);
-                    if (index < files.Count && string.IsNullOrEmpty(name)) {
-                        missingFIleList.Items.Add((firstUnfound ? "" : "  ") + fileName + $" ({(Emotion)emoteCount})");
-                        itemsNotFound = true;
-                        firstUnfound = false;
-                    } else {
-                        firstUnfound = true;
-                        foundNamesList.Items.Add(Path.GetFileName(name) + $" ({(Emotion)emoteCount})");
-                        secondaryKnownFileList.Add(name);
+            if (!dontLoad) {
+                missingFIleList.Items.Clear();
+                if (files != null) {
+                    files.Clear();
+                }
+                files = new List<string>();
+                files.AddRange(Directory.GetFiles(dumpFilepath));
+                if (secondaryKnownFileList != null) {
+                    secondaryKnownFileList.Clear();
+                }
+                secondaryKnownFileList = new List<string>();
+                int startValue = 7301000;
+                int index = 0;
+                int attempts = (files.Count / 16);
+                bool firstUnfound = false;
+                int foundVoices = 0;
+                int unfoundVoices = 0;
+                for (int counter = 0; counter <= attempts; counter++) {
+                    bool itemsNotFound = false;
+                    for (int emoteCount = 0; emoteCount <= 15; emoteCount++) {
+                        string fileName = (startValue + emoteCount) + "";
+                        string name = ListContainsName(files, fileName);
+                        if (string.IsNullOrEmpty(name)) {
+                            missingFIleList.Items.Add((firstUnfound ? "" : "  ") + fileName + $" ({(Emotion)emoteCount})");
+                            itemsNotFound = true;
+                            firstUnfound = false;
+                        } else {
+                            firstUnfound = true;
+                            index++;
+                            foundNamesList.Items.Add(Path.GetFileName(name) + $" ({(Emotion)emoteCount})");
+                            secondaryKnownFileList.Add(name);
+                        }
                     }
+                    if (itemsNotFound) {
+                        missingFIleList.Items.Add("----------------");
+                        startNumberGuess.Items.Add(startValue);
+                        unfoundVoices++;
+                    } else {
+                        foundNamesList.Items.Add("----------------");
+                        startNumber.Items.Add(startValue);
+                        foundVoices++;
+                        secondaryKnownFileList.Add("");
+                    }
+                    startValue += 1000;
                 }
-                if (itemsNotFound) {
-                    missingFIleList.Items.Add("----------------");
-                    startNumberGuess.Items.Add(startValue);
-                    unfoundVoices++;
-                } else {
-                    foundNamesList.Items.Add("----------------");
-                    startNumber.Items.Add(startValue);
-                    foundVoices++;
-                    secondaryKnownFileList.Add("");
+                lostFileListPaths = new List<string>();
+                while (index < files.Count) {
+                    lostFileList.Items.Add(Path.GetFileName(files[index++]));
+                    lostFileListPaths.Add(files[index++]);
                 }
-                startValue += 1000;
-            }
-            MessageBox.Show("Found " + foundVoices + " voices. Was not able to find " + unfoundVoices + " voices.", "FFXIV Voice Clip Name Guesser");
-            if (missingFIleList.Items.Count > 0) {
-                MessageBox.Show(missingFIleList.Items.Count + @" items do not have a known filename. Check ""Missing Names"" list to help with you guesswork", "FFXIV Voice Clip Name Guesser");
+                MessageBox.Show("Found " + foundVoices + " voices. Was not able to find " + unfoundVoices + " voices.", "FFXIV Voice Clip Name Guesser");
+                if (missingFIleList.Items.Count > 0) {
+                    MessageBox.Show(missingFIleList.Items.Count + @" items do not have a known filename. Check ""Missing Names"" list to help with you guesswork", "FFXIV Voice Clip Name Guesser");
+                }
             }
         }
 
@@ -109,7 +116,7 @@ namespace FFXIVVoiceClipNameGuesser {
 
         private void incrementButton_Click(object sender, EventArgs e) {
             if (string.IsNullOrEmpty(startNumberGuess.Text)) {
-                startNumberGuess.SelectedIndex = 0;
+                startNumberGuess.SelectedIndex = -1;
             }
             string paths = "";
             List<string> files = new List<string>() {
@@ -129,7 +136,9 @@ namespace FFXIVVoiceClipNameGuesser {
                 happy.FilePath.Text,
                 unknown1.FilePath.Text,
                 unknown2.FilePath.Text};
-            GetFilePaths();
+            if (filepath == null) {
+                GetFilePaths();
+            }
             if (filepath != null) {
                 bool firstDone = false;
                 for (int counter = 0; counter <= int.Parse(attempts.Text); counter++) {
@@ -163,7 +172,8 @@ namespace FFXIVVoiceClipNameGuesser {
 
         private void generateButton_Click(object sender, EventArgs e) {
             if (string.IsNullOrEmpty(startNumber.Text)) {
-                startNumber.SelectedIndex = 0;
+                startNumber.SelectedIndex = -1;
+                startNumber.Text = 7301000 + "";
             }
             string paths = "";
             List<string> files = new List<string>() {
@@ -183,7 +193,9 @@ namespace FFXIVVoiceClipNameGuesser {
                 happy.FilePath.Text,
                 unknown1.FilePath.Text,
                 unknown2.FilePath.Text};
-            GetFilePaths();
+            if (filepath == null) {
+                GetFilePaths();
+            }
             if (filepath != null) {
                 bool firstDone = false;
                 for (int counter = 0; counter <= int.Parse(attempts.Text); counter++) {
@@ -225,38 +237,35 @@ namespace FFXIVVoiceClipNameGuesser {
             }
         }
 
-        private void GetFilePaths() {
-            if (filepath == null) {
-                MessageBox.Show(@"Please select the destination folder inside your custom penumbra sound mod. Should be under ""sound\voice\vo_emote""");
-                FolderSelectDialog folderSelect = new FolderSelectDialog();
-                if (folderSelect.ShowDialog() == DialogResult.OK) {
-                    filepath = folderSelect.SelectedPath;
-                    if (!filepath.Contains("vo_emote")) {
-                        MessageBox.Show(@"This is not a valid location, cancelling");
-                        filepath = null;
-                        return;
-                    }
-                    if (jsonFilepath == null) {
-                        MessageBox.Show(@"Please select the ""default_mod.json"" file or equivalent in the root of your custom penumbra sound mod.");
-                        OpenFileDialog openFileDialog = new OpenFileDialog();
-                        openFileDialog.Filter = "json files (*.json)|*.json";
-                        if (openFileDialog.ShowDialog() == DialogResult.OK) {
-                            jsonFilepath = openFileDialog.FileName;
-                        }
-                    }
+        private void ExportMeta() {
+            string metaText = @"{
+  ""FileVersion"": 3,
+  ""Name"": """ + modNameTextbox.Text + @""",
+  ""Author"": """ + authorTextBox.Text + @""",
+  ""Description"": """ + descriptionTextBox.Text + @""",
+  ""Version"": """ + modVersionTextBox.Text + @""",
+  ""Website"": """ + modWebsiteTextBox.Text + @""",
+  ""ModTags"": []
+}";
+            if (metaFilePath != null) {
+                using (StreamWriter writer = new StreamWriter(metaFilePath)) {
+                    writer.WriteLine(metaText);
                 }
             }
         }
 
-        private void jsonText_TextChanged(object sender, EventArgs e) {
-
-        }
-
-        private void label4_Click(object sender, EventArgs e) {
-
-        }
-
-        private void analyzeVoiceDumpButton_Click(object sender, EventArgs e) {
+        private void GetFilePaths() {
+            MessageBox.Show(@"Please select the root folder for your custom penumbra sound mod. Ideally you've created one inside your penumbra mods folder for easy testing. (You can use Penumbra to export this mod to a .pmp package later)", "FFXIV Voice Clip Name Guesser");
+            FolderSelectDialog folderSelect = new FolderSelectDialog();
+            if (folderSelect.ShowDialog() == DialogResult.OK) {
+                filepath = Path.Combine(folderSelect.SelectedPath, @"\sound\voice\vo_emote");
+                Directory.CreateDirectory(filepath);
+                jsonFilepath = Path.Combine(folderSelect.SelectedPath, "default_mod.json");
+                metaFilePath = Path.Combine(folderSelect.SelectedPath, "meta.json");
+                if (File.Exists(jsonFilepath)) {
+                    MessageBox.Show(@"Existing mod configuration detected.", "FFXIV Voice Clip Name Guesser");
+                }
+            }
         }
 
         private string ListContainsName(List<string> files, string fileName) {
@@ -306,19 +315,30 @@ namespace FFXIVVoiceClipNameGuesser {
                 canDoDragDrop = false;
             }
         }
+        private void lostNamesList_MouseDown(object sender, MouseEventArgs e) {
+            startPos = e.Location;
+            canDoDragDrop = true;
+        }
 
+        private void lostNamesList_MouseMove(object sender, MouseEventArgs e) {
+            if ((e.X != startPos.X || startPos.Y != e.Y) && canDoDragDrop) {
+                List<string> fileList = new List<string>();
+                foreach (int item in lostFileList.SelectedIndices) {
+                    if (!string.IsNullOrEmpty(lostFileListPaths[item])) {
+                        fileList.Add(lostFileListPaths[item]);
+                    }
+                }
+                if (fileList.Count > 0) {
+                    DataObject fileDragData = new DataObject(DataFormats.FileDrop, fileList.ToArray());
+                    DoDragDrop(fileDragData, DragDropEffects.Copy);
+                }
+                canDoDragDrop = false;
+            }
+        }
         private void foundNamesList_MouseDoubleClick(object sender, MouseEventArgs e) {
             if (!string.IsNullOrEmpty(secondaryKnownFileList[foundNamesList.SelectedIndex])) {
                 Process.Start(secondaryKnownFileList[foundNamesList.SelectedIndex]);
             }
-        }
-
-        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e) {
-
-        }
-
-        private void startValueGuess_SelectedIndexChanged(object sender, EventArgs e) {
-
         }
 
         private void sCDWavMergerToolStripMenuItem_Click(object sender, EventArgs e) {
@@ -328,6 +348,23 @@ namespace FFXIVVoiceClipNameGuesser {
 
         private void changeVoiceDumpToolStripMenuItem_Click(object sender, EventArgs e) {
             PickVoiceDump();
+        }
+
+        private void pickExportFolderToolStripMenuItem_Click(object sender, EventArgs e) {
+            GetFilePaths();
+        }
+
+        private void lostFileList_MouseDoubleClick(object sender, MouseEventArgs e) {
+            if (!string.IsNullOrEmpty(lostFileListPaths[lostFileList.SelectedIndex])) {
+                Process.Start(lostFileListPaths[lostFileList.SelectedIndex]);
+            }
+        }
+
+        private void lostFileList_SelectedIndexChanged(object sender, EventArgs e) {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e) {
         }
     }
 }
--- FFXIVVoiceClipNameGuesser/Properties/AssemblyInfo.cs
+++ FFXIVVoiceClipNameGuesser/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("FFXIVVoiceClipNameGuesser")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Find voices and create custom sound packs.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Lawener Industries")]
 [assembly: AssemblyProduct("FFXIVVoiceClipNameGuesser")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.0.0.0")]
-[assembly: AssemblyFileVersion("0.0.0.0")]
+[assembly: AssemblyVersion("0.0.0.4")]
+[assembly: AssemblyFileVersion("0.0.0.4")]
