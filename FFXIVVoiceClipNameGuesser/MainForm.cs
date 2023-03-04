b0362b1e87705daa4c6675d8b79cab6076e2b00e
Sebastian Lawe <sebane1@thavea.com>
2023-03-04 15:53:41 -0600

    Further tweaks to Au Ra voice 10

--- FFXIVVoiceClipNameGuesser/MainForm.cs
+++ FFXIVVoiceClipNameGuesser/MainForm.cs
@@ -726,9 +726,9 @@ namespace FFXIVVoicePackCreator {
                                     // Au Ra sorting
                                     battleSounds = new List<string>() {
                                     // Hurt
-                                    list[6],list[7],list[8], list[9], list[10],
+                                    list[6],list[13],list[8], list[9], list[10],
                                     // Death
-                                    list[12],list[13],
+                                    list[12],list[7],
                                     // Attack
                                     list[0],list[1],list[2],list[3],list[4],list[5],
                                     // Extra
